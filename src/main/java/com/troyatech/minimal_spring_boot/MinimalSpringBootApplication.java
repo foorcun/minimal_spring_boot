c8c71dc28d32eb24d0aa31074ef1e74bac64449e
foorcun <msnifoorcun@hotmail.com>
2022-11-19 18:21:20 +0300

    first push

---
+++ src/main/java/com/troyatech/minimal_spring_boot/MinimalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.troyatech.minimal_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/troyatech/minimal_spring_boot/MinimalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troyatech.minimal_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
